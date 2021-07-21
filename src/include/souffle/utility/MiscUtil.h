6f37c54b57267161764c83605cd4543b61f46627
Quentin Sabah <quentin.sabah@gmail.com>
2021-07-21 22:57:24 +0200

    intrinsics

--- src/include/souffle/utility/MiscUtil.h
+++ src/include/souffle/utility/MiscUtil.h
@@ -54,6 +54,8 @@
 #define __builtin_popcountll __popcnt64
 
 #if defined(_MSC_VER)
+#include <intrin.h>
+#pragma intrinsic(_BitScanForward64)
 constexpr unsigned long __builtin_ctz(unsigned long value) {
     unsigned long trailing_zeroes = 0;
     while ((value = value >> 1) ^ 1) {
@@ -62,7 +64,7 @@ constexpr unsigned long __builtin_ctz(unsigned long value) {
     return trailing_zeroes;
 }
 
-inline int __builtin_ctzll(unsigned long long value) {
+constexpr int __builtin_ctzll(unsigned long long value) {
     unsigned long trailing_zero = 0;
 
     if (_BitScanForward64(&trailing_zero, value)) {
