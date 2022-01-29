2d888ec12b66513968739c07d32ffb600c2ac4ba
Quentin Sabah <quentin.sabah@gmail.com>
2022-01-29 14:57:25 +0100

    main driver interface

--- src/MainDriver.h
+++ src/MainDriver.h
@@ -1,3 +1,18 @@
+/*
+ * Souffle - A Datalog Compiler
+ * Copyright (c) 2022, The Souffle Developers. All rights reserved
+ * Licensed under the Universal Permissive License v 1.0 as shown at:
+ * - https://opensource.org/licenses/UPL
+ * - <souffle root>/licenses/SOUFFLE-UPL.txt
+ */
+
+/************************************************************************
+ *
+ * @file MainDriver.hpp
+ *
+ * Main driver interface for Souffle
+ *
+ ***********************************************************************/
 
 #pragma once
 
@@ -10,11 +25,26 @@
 #include "ram/transform/Transformer.h"
 
 namespace souffle {
+
+/** Return the package version string. */
 const char* packageVersion();
+
+/** Return the command-line options that can be passed to `MainConfig::processArgs()`. */
+std::vector<MainOption> getMainOptions();
+
+/** Execute entire Souffle driver. */
 int main(Global& glb, const char* souffle_executable);
+
+/** Construct and return an AST transformer pipeline. */
 Own<ast::transform::PipelineTransformer> astTransformationPipeline(Global& glb);
+
+/** Construct and return an AST to RAM unit translator. */
 Own<ast2ram::UnitTranslator> getUnitTranslator(Global& glb);
+
+/** Construct and return a RAM transformer pipeline. */
 Own<ram::transform::Transformer> ramTransformerSequence(Global& glb);
+
+/** Interpret the RAM translation unit using Souffle engine. */
 bool interpretTranslationUnit(Global& glb, ram::TranslationUnit& ramTranslationUnit);
 
 }  // namespace souffle
--- src/souffle.cpp
+++ src/souffle.cpp
@@ -26,7 +26,7 @@ bool processArgs(Global& glb, int argc, char** argv) {
     footer << "----------------------------------------------------------------------------" << std::endl;
     footer << "Version: " << packageVersion() << "" << std::endl;
     footer << "----------------------------------------------------------------------------" << std::endl;
-    footer << "Copyright (c) 2016-21 The Souffle Developers." << std::endl;
+    footer << "Copyright (c) 2016-22 The Souffle Developers." << std::endl;
     footer << "Copyright (c) 2013-16 Oracle and/or its affiliates." << std::endl;
     footer << "All rights reserved." << std::endl;
     footer << "============================================================================" << std::endl;
@@ -34,65 +34,7 @@ bool processArgs(Global& glb, int argc, char** argv) {
     // command line options, the environment will be filled with the arguments passed to them, or
     // the empty string if they take none
     // main option, the datalog program itself, has an empty key
-    std::vector<MainOption> options{{"", 0, "", "", false, ""},
-            {"fact-dir", 'F', "DIR", ".", false, "Specify directory for fact files."},
-            {"include-dir", 'I', "DIR", ".", true, "Specify directory for include files."},
-            {"output-dir", 'D', "DIR", ".", false,
-                    "Specify directory for output files. If <DIR> is `-` then stdout is used."},
-            {"jobs", 'j', "N", "1", false,
-                    "Run interpreter/compiler in parallel using N threads, N=auto for system "
-                    "default."},
-            {"compile", 'c', "", "", false,
-                    "Generate C++ source code, compile to a binary executable, then run this "
-                    "executable."},
-            {"generate", 'g', "FILE", "", false,
-                    "Generate C++ source code for the given Datalog program and write it to "
-                    "<FILE>. If <FILE> is `-` then stdout is used."},
-            {"inline-exclude", '\x7', "RELATIONS", "", false,
-                    "Prevent the given relations from being inlined. Overrides any `inline` qualifiers."},
-            {"swig", 's', "LANG", "", false,
-                    "Generate SWIG interface for given language. The values <LANG> accepts is java and "
-                    "python. "},
-            {"library-dir", 'L', "DIR", "", true, "Specify directory for library files."},
-            {"libraries", 'l', "FILE", "", true, "Specify libraries."},
-            {"no-warn", 'w', "", "", false, "Disable warnings."},
-            {"magic-transform", 'm', "RELATIONS", "", false,
-                    "Enable magic set transformation changes on the given relations, use '*' "
-                    "for all."},
-            {"magic-transform-exclude", '\x8', "RELATIONS", "", false,
-                    "Disable magic set transformation changes on the given relations. Overrides "
-                    "`magic-transform`. Implies `inline-exclude` for the given relations."},
-            {"macro", 'M', "MACROS", "", false, "Set macro definitions for the pre-processor"},
-            {"disable-transformers", 'z', "TRANSFORMERS", "", false, "Disable the given AST transformers."},
-            {"dl-program", 'o', "FILE", "", false,
-                    "Generate C++ source code, written to <FILE>, and compile this to a "
-                    "binary executable (without executing it)."},
-            {"live-profile", '\1', "", "", false, "Enable live profiling."},
-            {"profile", 'p', "FILE", "", false, "Enable profiling, and write profile data to <FILE>."},
-            {"profile-use", 'u', "FILE", "", false,
-                    "Use profile log-file <FILE> for profile-guided optimization."},
-            {"profile-frequency", '\2', "", "", false, "Enable the frequency counter in the profiler."},
-            {"debug-report", 'r', "FILE", "", false, "Write HTML debug report to <FILE>."},
-            {"pragma", 'P', "OPTIONS", "", true, "Set pragma options."},
-            {"provenance", 't', "[ none | explain | explore ]", "", false,
-                    "Enable provenance instrumentation and interaction."},
-            {"verbose", 'v', "", "", false, "Verbose output."}, {"version", '\3', "", "", false, "Version."},
-            {"show", '\4', "[ <see-list> ]", "", true,
-                    "Print selected program information.\n"
-                    "Modes:\n"
-                    "\tinitial-ast\n"
-                    "\tinitial-ram\n"
-                    "\tparse-errors\n"
-                    "\tprecedence-graph\n"
-                    "\tprecedence-graph-text\n"
-                    "\tscc-graph\n"
-                    "\tscc-graph-text\n"
-                    "\ttransformed-ast\n"
-                    "\ttransformed-ram\n"
-                    "\ttype-analysis"},
-            {"parse-errors", '\5', "", "", false, "Show parsing errors, if any, then exit."},
-            {"help", 'h', "", "", false, "Display this help message."},
-            {"legacy", '\6', "", "", false, "Enable legacy support."}};
+    const std::vector<MainOption> options = getMainOptions();
     glb.config().processArgs(argc, argv, header.str(), footer.str(), options);
 
     return true;
